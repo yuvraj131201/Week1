3e43a661c815cf7b01e2ff4362bac0a5974ac2cd
yuvraj131201 <yuvrajjadeja2001@gmail.com>
2024-01-10 13:20:55 -0800

    Initial Commit

---
+++ Week1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Week1
+//
+//  Created by user239777 on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Week1/Week1App.swift
@@ -0,0 +1,17 @@
+//
+//  Week1App.swift
+//  Week1
+//
+//  Created by user239777 on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Week1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
